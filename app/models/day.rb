7ef7306c319485fb9b0a7641847971c46d7165bd
EC2 Default User <ec2-user@ip-172-31-22-20.us-east-2.compute.internal>
2019-04-15 03:22:25 +0000

    Updated Events

--- app/models/day.rb
+++ app/models/day.rb
@@ -1,2 +1,8 @@
 class Day < ApplicationRecord
+    has_many :events
+    
+    
+    def to_s
+        "#{title}"
+    end
 end
--- db/migrate/20190403232305_create_days.rb
+++ db/migrate/20190403232305_create_days.rb
@@ -2,10 +2,17 @@ class CreateDays < ActiveRecord::Migration[5.2]
   def change
     create_table :days do |t|
       t.string :title
-      t.string :text
       t.text :text
 
       t.timestamps
     end
+    
+     create_table :events do |t|
+      t.string :title
+      t.text :body
+
+      t.timestamps
+    end
+    
   end
 end
