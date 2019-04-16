afe2eb99f840674cad05c58fe42fee9553b6a028
EC2 Default User <ec2-user@ip-172-31-22-20.us-east-2.compute.internal>
2019-04-16 05:02:59 +0000

    Need to CSS Now

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -1,55 +1,51 @@
 class DaysController < ApplicationController
-   before_action :current_owner, only: [:show, :edit, :update, :destroy]
-   
-  def index
-    @days = Day.all
-  end
+    def index
+      @days = Day.all
+    end
 
-
-  def show
+    def show
     @day = Day.find(params[:id])
-  end
+    end
 
-  def new
-    @day = Day.new
-  end
+    def new
+      @day = Day.new
+    end
 
-  
-  
-  
-  def create
-    @day = Day.create(day_params)
-    if @day.save
-      redirect_to @day, alert: "Day created successfully."
+    def create
+      @day = Day.new(day_params)
+      
+      
+      if @day.save
+        redirect_to @day
+      else
+        render 'new'
+      end
+    end
+
+    def edit
+    @day = Day.find(params[:id])
+    end
+
+    def update
+      @day = Day.find(params[:id])
+      
+    if @day.update(day_params)
+    redirect_to @day
     else
-    redirect_to new_day_path, alert: "Error creating day"
+    render 'edit'
     end
-  end
+    end
 
-def edit
-  
-end
-  
-  
-  def update
-    @day.update(day_params)
-    
-    redirect_to day_path(@day)
-  end
-  
     def destroy
-     @day.destroy
-     redirect_to days_path
-    
+      @day = Day.find(params[:id])
+      @day.destroy
+      redirect_to days_path
+    end    
+end
+
+    private
+    def day_params
+      params.require(:day).permit(:title, :text)
     end
 
-  
-  private
-  def day_params
-    params.require(:day).permit(:title, :text)
-  end
-  
-  def current_owner
-    @day = Day.find(params[:id])
-  end
-end
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,19 @@ Rails.application.routes.draw do
   resources :days do
     resources :events
 end
+
+
+  get 'days/show'
+  get 'days/new'
+  get 'days/edit'
+  get 'events/new'
+  get 'events/index'
+  get 'events/show'
+  get 'events/edit'
+  get 'days/index'
+  post 'days/edit'
+  post 'events/edit'
+  get 'homepage/index'
   get 'homepage/index'
   root 'homepage#index'
 end
