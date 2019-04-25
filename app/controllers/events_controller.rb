8468cbfe61cc198bfc1471eb3575d0600d834ce7
EC2 Default User <ec2-user@ip-172-31-36-200.us-east-2.compute.internal>
2019-04-25 05:27:32 +0000

    Fixed the destroy function

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -38,7 +38,7 @@ class EventsController < ApplicationController
       end
 
       def destroy
-        @event. Event.find(params[:id])
+        @event = Event.find(params[:id])
         @event.destroy
         redirect_to events_path
       end
